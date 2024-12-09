d3b5bed6d9c4788a872e46c239e7eea4aafcda0e
니꼬 <nico@nomadcoders.co>
2024-12-09 10:26:36 +0900

    3.38 Teams Page

---
+++ app/features/teams/pages/team-page.tsx
@@ -0,0 +1,9 @@
+import { Route } from "./+types/team-page";
+
+export const meta: Route.MetaFunction = () => [
+  { title: "Team Details | wemake" },
+];
+
+export default function TeamPage() {
+  return <div></div>;
+}
