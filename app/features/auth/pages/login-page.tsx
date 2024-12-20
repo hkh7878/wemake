b4025d60d9e1859b5f6b0278846e4c73b0c282a6
니꼬 <nico@nomadcoders.co>
2024-12-20 12:59:12 +0900

    7.3 Action Function

--- app/features/auth/pages/login-page.tsx
+++ app/features/auth/pages/login-page.tsx
@@ -1,14 +1,27 @@
 import { Button } from "~/common/components/ui/button";
 import { Route } from "./+types/login-page";
-import { Form, Link } from "react-router";
+import { Form, Link, useNavigation } from "react-router";
 import InputPair from "~/common/components/input-pair";
 import AuthButtons from "../components/auth-buttons";
+import { LoaderCircle } from "lucide-react";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Login | wemake" }];
 };
 
-export default function LoginPage() {
+export const action = async ({ request }: Route.ActionArgs) => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  return {
+    message: "Error wrong password",
+  };
+};
+
+export default function LoginPage({ actionData }: Route.ComponentProps) {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <div className="flex flex-col relative items-center justify-center h-full">
       <Button variant={"ghost"} asChild className="absolute right-8 top-8 ">
@@ -16,7 +29,7 @@ export default function LoginPage() {
       </Button>
       <div className="flex items-center flex-col justify-center w-full max-w-md gap-10">
         <h1 className="text-2xl font-semibold">Log in to your account</h1>
-        <Form className="w-full space-y-4">
+        <Form className="w-full space-y-4" method="post">
           <InputPair
             label="Email"
             description="Enter your email address"
@@ -35,9 +48,16 @@ export default function LoginPage() {
             type="password"
             placeholder="i.e wemake@example.com"
           />
-          <Button className="w-full" type="submit">
-            Log in
+          <Button className="w-full" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              "Log in"
+            )}
           </Button>
+          {actionData?.message && (
+            <p className="text-sm text-red-500">{actionData.message}</p>
+          )}
         </Form>
         <AuthButtons />
       </div>
