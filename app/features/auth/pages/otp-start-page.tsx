f6184864ea1598b57397fe0df3386aa98c852d0b
니꼬 <nico@nomadcoders.co>
2024-12-24 08:37:15 +0900

    7.9 OTP Login

--- app/features/auth/pages/otp-start-page.tsx
+++ app/features/auth/pages/otp-start-page.tsx
@@ -1,14 +1,48 @@
 import { Button } from "~/common/components/ui/button";
 import { Input } from "~/common/components/ui/input";
 import { Route } from "./+types/otp-start-page";
-import { Form } from "react-router";
+import { Form, redirect, useNavigate, useNavigation } from "react-router";
 import InputPair from "~/common/components/input-pair";
+import { z } from "zod";
+import { makeSSRClient } from "~/supa-client";
+import { LoaderCircle } from "lucide-react";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Start OTP | wemake" }];
 };
 
-export default function OtpStartPage() {
+const formSchema = z.object({
+  email: z.string().email(),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { data, success } = formSchema.safeParse(Object.fromEntries(formData));
+  if (!success) {
+    return { error: "Invalid email address" };
+  }
+  const { email } = data;
+
+  const { client } = makeSSRClient(request);
+
+  const { error } = await client.auth.signInWithOtp({
+    email,
+    options: {
+      shouldCreateUser: true,
+    },
+  });
+
+  if (error) {
+    return { error: "Failed to send OTP" };
+  }
+
+  return redirect(`/auth/otp/complete?email=${email}`);
+};
+
+export default function OtpStartPage({ actionData }: Route.ComponentProps) {
+  const navigation = useNavigation();
+  const isSubmitting =
+    navigation.state === "submitting" || navigation.state === "loading";
   return (
     <div className="flex flex-col relative items-center justify-center h-full">
       <div className="flex items-center flex-col justify-center w-full max-w-md gap-10">
@@ -18,7 +52,7 @@ export default function OtpStartPage() {
             We will send you a 4-digit code to log in to your account.
           </p>
         </div>
-        <Form className="w-full space-y-4">
+        <Form className="w-full space-y-4" method="post">
           <InputPair
             label="Email"
             description="Enter your email address"
@@ -28,8 +62,15 @@ export default function OtpStartPage() {
             type="email"
             placeholder="i.e wemake@example.com"
           />
-          <Button className="w-full" type="submit">
-            Send OTP
+          {actionData && "error" in actionData && (
+            <p className="text-red-500 text-sm">{actionData.error}</p>
+          )}
+          <Button className="w-full" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              "Send OTP"
+            )}
           </Button>
         </Form>
       </div>
