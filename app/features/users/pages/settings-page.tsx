81d6b1c720129cdbf5209f30c8016c7928c5314f
니꼬 <nico@nomadcoders.co>
2024-12-31 09:15:21 +0900

    8.9 Edit Profile

--- app/features/users/pages/settings-page.tsx
+++ app/features/users/pages/settings-page.tsx
@@ -6,12 +6,66 @@ import { useState } from "react";
 import { Label } from "~/common/components/ui/label";
 import { Input } from "~/common/components/ui/input";
 import { Button } from "~/common/components/ui/button";
+import { getLoggedInUserId, getUserById } from "../queries";
+import { makeSSRClient } from "~/supa-client";
+import { z } from "zod";
+import { updateUser } from "../mutations";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "~/common/components/ui/alert";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Settings | wemake" }];
 };
 
-export default function SettingsPage() {
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  const { client } = makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  const user = await getUserById(client, { id: userId });
+  return { user };
+};
+
+const formSchema = z.object({
+  name: z.string().min(3),
+  role: z.string(),
+  headline: z.string().optional().default(""),
+  bio: z.string().optional().default(""),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const { client } = makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  const formData = await request.formData();
+  const { success, error, data } = formSchema.safeParse(
+    Object.fromEntries(formData)
+  );
+  if (!success) {
+    return { formErrors: error.flatten().fieldErrors };
+  }
+  const { name, role, headline, bio } = data;
+  await updateUser(client, {
+    id: userId,
+    name,
+    role: role as
+      | "developer"
+      | "designer"
+      | "marketer"
+      | "founder"
+      | "product-manager",
+    headline,
+    bio,
+  });
+  return {
+    ok: true,
+  };
+};
+
+export default function SettingsPage({
+  loaderData,
+  actionData,
+}: Route.ComponentProps) {
   const [avatar, setAvatar] = useState<string | null>(null);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -23,18 +77,36 @@ export default function SettingsPage() {
     <div className="space-y-20">
       <div className="grid grid-cols-6 gap-40">
         <div className="col-span-4 flex flex-col gap-10">
+          {actionData?.ok ? (
+            <Alert>
+              <AlertTitle>Success</AlertTitle>
+              <AlertDescription>
+                Your profile has been updated.
+              </AlertDescription>
+            </Alert>
+          ) : null}
           <h2 className="text-2xl font-semibold">Edit profile</h2>
-          <Form className="flex flex-col w-1/2 gap-5">
+          <Form className="flex flex-col w-1/2 gap-5" method="post">
             <InputPair
               label="Name"
               description="Your public name"
               required
               id="name"
+              defaultValue={loaderData.user.name}
               name="name"
               placeholder="John Doe"
             />
+            {actionData?.formErrors?.name ? (
+              <Alert>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {actionData.formErrors.name.join(", ")}
+                </AlertDescription>
+              </Alert>
+            ) : null}
             <SelectPair
               label="Role"
+              defaultValue={loaderData.user.role}
               description="What role do you do identify the most with"
               name="role"
               placeholder="Select a role"
@@ -43,27 +115,53 @@ export default function SettingsPage() {
                 { label: "Designer", value: "designer" },
                 { label: "Product Manager", value: "product-manager" },
                 { label: "Founder", value: "founder" },
-                { label: "Other", value: "other" },
+                { label: "Marketer", value: "marketer" },
               ]}
             />
+            {actionData?.formErrors?.role ? (
+              <Alert>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {actionData.formErrors.role.join(", ")}
+                </AlertDescription>
+              </Alert>
+            ) : null}
             <InputPair
               label="Headline"
               description="An introduction to your profile."
               required
+              defaultValue={loaderData.user.headline ?? ""}
               id="headline"
               name="headline"
               placeholder="John Doe"
               textArea
             />
+            {actionData?.formErrors?.headline ? (
+              <Alert>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {actionData.formErrors.headline.join(", ")}
+                </AlertDescription>
+              </Alert>
+            ) : null}
             <InputPair
               label="Bio"
               description="Your public bio. It will be displayed on your profile page."
               required
               id="bio"
+              defaultValue={loaderData.user.bio ?? ""}
               name="bio"
               placeholder="John Doe"
               textArea
             />
+            {actionData?.formErrors?.bio ? (
+              <Alert>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {actionData.formErrors.bio.join(", ")}
+                </AlertDescription>
+              </Alert>
+            ) : null}
             <Button className="w-full">Update profile</Button>
           </Form>
         </div>
