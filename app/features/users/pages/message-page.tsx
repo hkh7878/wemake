73566ff6a5728165c42890b189716954f6b04037
니꼬 <nico@nomadcoders.co>
2025-01-03 11:15:41 +0900

    10.3 Fetching Messages

--- app/features/users/pages/message-page.tsx
+++ app/features/users/pages/message-page.tsx
@@ -10,39 +10,68 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "~/common/components/ui/avatar";
-import { Form } from "react-router";
+import { Form, useOutletContext } from "react-router";
 import { Textarea } from "~/common/components/ui/textarea";
 import { Button } from "~/common/components/ui/button";
 import { SendIcon } from "lucide-react";
 import { MessageBubble } from "../components/message-bubble";
+import {
+  getLoggedInUserId,
+  getMessagesByMessagesRoomId,
+  getRoomsParticipant,
+} from "../queries";
+import { getMessages } from "../queries";
+import { makeSSRClient } from "~/supa-client";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Message | wemake" }];
 };
 
-export default function MessagePage() {
+export const loader = async ({ request, params }: Route.LoaderArgs) => {
+  const { client } = await makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  const messages = await getMessagesByMessagesRoomId(client, {
+    messageRoomId: params.messageRoomId,
+    userId,
+  });
+  const participants = await getRoomsParticipant(client, {
+    messageRoomId: params.messageRoomId,
+    userId,
+  });
+  return {
+    messages,
+    participants,
+  };
+};
+
+export default function MessagePage({ loaderData }: Route.ComponentProps) {
+  const { userId } = useOutletContext<{ userId: string }>();
   return (
     <div className="h-full flex flex-col justify-between">
       <Card>
         <CardHeader className="flex flex-row items-center gap-4">
           <Avatar className="size-14">
-            <AvatarImage src="https://github.com/stevejobs.png" />
-            <AvatarFallback>S</AvatarFallback>
+            <AvatarImage src={loaderData.participants?.profile?.avatar ?? ""} />
+            <AvatarFallback>
+              {loaderData.participants?.profile?.name.charAt(0) ?? ""}
+            </AvatarFallback>
           </Avatar>
           <div className="flex flex-col gap-0">
-            <CardTitle className="text-xl">Steve Jobs</CardTitle>
+            <CardTitle className="text-xl">
+              {loaderData.participants?.profile?.name ?? ""}
+            </CardTitle>
             <CardDescription>2 days ago</CardDescription>
           </div>
         </CardHeader>
       </Card>
-      <div className="py-10 overflow-y-scroll flex flex-col justify-start h-full">
-        {Array.from({ length: 10 }).map((_, index) => (
+      <div className="py-10 overflow-y-scroll space-y-4 flex flex-col justify-start h-full">
+        {loaderData.messages.map((message) => (
           <MessageBubble
-            key={index}
-            avatarUrl="https://github.com/stevejobs.png"
-            avatarFallback="S"
-            content="this is a message from steve jobs in iheaven, make sure to reply because if you don't, you will be punished."
-            isCurrentUser={index % 2 === 0}
+            key={message.message_id}
+            avatarUrl={message.sender?.avatar ?? ""}
+            avatarFallback={message.sender?.name.charAt(0) ?? ""}
+            content={message.content}
+            isCurrentUser={message.sender?.profile_id === userId}
           />
         ))}
       </div>
