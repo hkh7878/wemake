6fd0938a9e3ad60f60adfcb9ec08d4352b930e78
니꼬 <nico@nomadcoders.co>
2025-01-08 11:15:43 +0900

    13.2 Request Payment

--- app/features/products/pages/promote-page.tsx
+++ app/features/products/pages/promote-page.tsx
@@ -3,15 +3,19 @@ import { Route } from "./+types/promote-page";
 import { Form } from "react-router";
 import SelectPair from "~/common/components/select-pair";
 import { Calendar } from "~/common/components/ui/calendar";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Label } from "~/common/components/ui/label";
 import { DateRange } from "react-day-picker";
 import { DateTime } from "luxon";
 import { Button } from "~/common/components/ui/button";
+import {
+  loadTossPayments,
+  TossPaymentsWidgets,
+} from "@tosspayments/tosspayments-sdk";
 
 export const meta: Route.MetaFunction = () => {
   return [
-    { title: "Promote Product | ProductHunt Clone" },
+    { title: "PromoteProduct | ProductHunt Clone" },
     { name: "description", content: "Promote your product" },
   ];
 };
@@ -27,15 +31,72 @@ export default function PromotePage() {
           "days"
         ).days
       : 0;
+  const widgets = useRef<TossPaymentsWidgets | null>(null);
+  const initedToss = useRef<boolean>(false);
+  useEffect(() => {
+    const initToss = async () => {
+      if (initedToss.current) return;
+      initedToss.current = true;
+      const toss = await loadTossPayments(
+        "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm"
+      );
+      widgets.current = await toss.widgets({
+        customerKey: "1111111",
+      });
+      await widgets.current.setAmount({
+        value: 0,
+        currency: "KRW",
+      });
+      await widgets.current.renderPaymentMethods({
+        selector: "#toss-payment-methods",
+      });
+      await widgets.current.renderAgreement({
+        selector: "#toss-payment-agreement",
+      });
+    };
+    initToss();
+  }, []);
+  useEffect(() => {
+    const updateAmount = async () => {
+      if (widgets.current) {
+        await widgets.current.setAmount({
+          value: totalDays * 20000,
+          currency: "KRW",
+        });
+      }
+    };
+    updateAmount();
+  }, [promotionPeriod]);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const product = formData.get("product") as string;
+    if (!product || !promotionPeriod?.to || !promotionPeriod?.from) return;
+    await widgets.current?.requestPayment({
+      orderId: crypto.randomUUID(),
+      orderName: `WeMake Promotion`,
+      customerEmail: "nico@nomadcoders.co",
+      customerName: "Nico",
+      customerMobilePhone: "01012345678",
+      metadata: {
+        product,
+        promotionFrom: DateTime.fromJSDate(promotionPeriod.from).toISO(),
+        promotionTo: DateTime.fromJSDate(promotionPeriod.to).toISO(),
+      },
+      successUrl: `${window.location.href}/success`,
+      failUrl: `${window.location.href}/fail`,
+    });
+  };
   return (
     <div>
       <Hero
         title="Promote Your Product"
         subtitle="Boost your product's visibility."
       />
-      <div className="grid grid-cols-6">
-        <Form className="col-span-4 mx-auto flex flex-col gap-10 items-center">
+      <form onSubmit={handleSubmit} className="grid grid-cols-6 gap-10">
+        <div className="col-span-3 mx-auto w-1/2 flex flex-col gap-10 items-start">
           <SelectPair
+            required
             label="Select a product"
             description="Select the product you want to promote."
             name="product"
@@ -44,14 +105,6 @@ export default function PromotePage() {
               {
                 label: "AI Dark Mode Maker",
                 value: "ai-dark-mode-maker",
-              },
-              {
-                label: "AI Dark Mode Maker",
-                value: "ai-dark-mode-maker-1",
-              },
-              {
-                label: "AI Dark Mode Maker",
-                value: "ai-dark-mode-maker-2",
               },
             ]}
           />
@@ -70,12 +123,20 @@ export default function PromotePage() {
               disabled={{ before: new Date() }}
             />
           </div>
-          <Button disabled={totalDays === 0}>
-            Go to checkout (${totalDays * 20})
+        </div>
+        <aside className="col-span-3 px-20 flex flex-col items-center">
+          <div id="toss-payment-methods" className="w-full" />
+          <div id="toss-payment-agreement" />
+          <Button className="w-full" disabled={totalDays === 0}>
+            Checkout (
+            {(totalDays * 20000).toLocaleString("ko-KR", {
+              style: "currency",
+              currency: "KRW",
+            })}
+            )
           </Button>
-        </Form>
-        <aside className="col-span-2"></aside>
-      </div>
+        </aside>
+      </form>
     </div>
   );
 }
